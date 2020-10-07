f52081d0ec27a962249133ef71d75e3cf8ec61bf
EC2 Default User <ec2-user@ip-172-31-12-7.ec2.internal>
2020-10-07 04:25:37 +0000

    Delete unused env vars in py

--- app.py
+++ app.py
@@ -17,10 +17,6 @@ socketio.init_app(app, cors_allowed_origins="*")
 dotenv_path = join(dirname(__file__), 'sql.env')
 load_dotenv(dotenv_path)
 
-sql_user = os.environ['SQL_USER']
-sql_pwd = os.environ['SQL_PASSWORD']
-dbuser = os.environ['USER']
-
 database_uri = os.environ['DATABASE_URL']
 
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
