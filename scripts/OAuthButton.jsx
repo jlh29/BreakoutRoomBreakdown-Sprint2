0e0b6289a442f50af6957091b2732f72b9505dd2
EC2 Default User <ec2-user@ip-172-31-12-7.ec2.internal>
2020-10-08 20:03:28 +0000

    Remove unused code

--- scripts/OAuthButton.jsx
+++ scripts/OAuthButton.jsx
@@ -1,18 +1,6 @@
 import * as React from 'react';
 import { Socket } from './Socket';
 
-function handleSubmit(event) {
-    let newAddress = document.getElementById("address_input");
-    Socket.emit('new address input', {
-        'address': newAddress.value,
-    });
-    
-    console.log('Sent the address ' + newAddress.value + ' to server!');
-    newAddress.value = ''
-    
-    event.preventDefault();
-}
-
 export function OAuthButton() {
     return (
             <button>Replace me!</button>
