92c077fd0b4859fedafcdbbdbce7325d3fa44118
ts549njit <ts549@njit.edu>
2020-11-07 20:40:01 +0000

    Work on adding Attendee and Appointment models to models.py

--- models.py
+++ models.py
@@ -14,6 +14,33 @@ class AuthUser(DB.Model):
         
     def __repr__(self):
         return "<User name: {}\ntype: {}".format(self.name, self.auth_type)
+        
+class Attendee(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    attendee = DB.Column(DB.String(120))
+    
+    appointmentID = DB.Column(DB.Integer, DB.ForeignKey("appointment.id"), nullable=False)
+    
+    def __init__(self, attendee, appointmentID):
+        self.attendee = attendee
+        self.appointmentID = appointmentID
+        
+class Appointment(DB.Model):
+    id = DB.Column(DB.Integer, primary_key=True)
+    organizer = DB.Column(DB.String(120))
+    numberOfAttendees = DB.Column(DB.Integer)
+    roomSize = DB.Column(DB.String(2))
+    # roomNumber = DB.Column(DB.Integer)
+    reservation = DB.Column(DB.DateTime())
+    
+    attendees = DB.relationship(Attendee, backref="Appointment", lazy="True")
+    
+    def __init__(self, organizer, numberOfAttendees, roomSize, reservation):
+        self.organizer = organizer
+        self.numberOfAttendees = numberOfAttendees
+        self.roomSize = roomSize
+        self.reservation = reservation
+    
 
 class AuthUserType(Enum):
     GOOGLE = "google"
