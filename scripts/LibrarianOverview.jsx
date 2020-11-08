553698eb5d29070812f4fb3e7843c70b05305d05
jlh29 <jlh29@njit.edu>
2020-11-08 07:10:42 +0000

    Create and render simple Librarian Overview page

---
+++ scripts/LibrarianOverview.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export default function LibrarianOverview() {
+    return (
+        <div>
+            <h1>View/Edit Appointments: TODO</h1>
+            <h1>View/Edit Rooms: TODO</h1>
+            <h1>Manual check in: TODO</h1>
+            <h1>View/Edit Professor List: TODO</h1>
+            <h1>View/Edit Calendar</h1>
+        </div>
+    );
+}
