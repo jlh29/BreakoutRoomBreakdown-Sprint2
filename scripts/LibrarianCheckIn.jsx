abe65bf1c6445c3cd84f035f0d68c2c4d5f4fa5b
jlh29 <jlh29@njit.edu>
2020-11-09 10:03:42 +0000

    Add check-in result 'toast' for when a reservation is (un)successfully checked in

--- scripts/LibrarianCheckIn.jsx
+++ scripts/LibrarianCheckIn.jsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
 import LibrarianCheckInInput from './LibrarianCheckInInput';
 import LibrarianCheckInSubmit from './LibrarianCheckInSubmit';
+import LibrarianCheckInResult from './LibrarianCheckInResult';
 
 export default function LibrarianCheckIn(props) {
-    const { inputRef, submitClick } = props;
+    const { inputRef, submitClick, showResult, isSuccess } = props;
     return (
         <div>
             <LibrarianCheckInInput inputRef={inputRef} />
             <LibrarianCheckInSubmit submitClick={submitClick} />
+            {showResult ?? <LibrarianCheckInResult isSuccess={isSuccess} /> }
         </div>
     );
 }
