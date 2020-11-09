915d370ae57d09470f1e8a9230e67b48ade925be
jlh29 <jlh29@njit.edu>
2020-11-09 10:45:34 +0000

    Add small bugfix to remove children from and add id to input element

--- scripts/LibrarianCheckInInput.jsx
+++ scripts/LibrarianCheckInInput.jsx
@@ -2,5 +2,12 @@ import * as React from 'react';
 
 export default function LibrarianCheckInInput(props) {
     const { inputRef } = props;
-    return <input type='text' ref={inputRef}>Check-in ID:</input>;
+    return (
+            <input
+                id='librarianCheckInInput'
+                type='text'
+                ref={inputRef}
+                placeholder='Check-in ID:'
+            ></input>
+    );
 }
