81607c1ff8d3904784dbbf867fa5a16bf2165fc1
jlh29 <jlh29@njit.edu>
2020-11-09 11:38:51 +0000

    Add and render new Appointments Overview component in the Librarian Overview with data obtained from the server

--- scripts/LibrarianOverview.jsx
+++ scripts/LibrarianOverview.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import LibrarianCheckIn from './LibrarianCheckIn';
+import LibrarianAppointmentsOverview from './LibrarianAppointmentsOverview';
 import Socket from './Socket';
 
 export default function LibrarianOverview() {
+    const [appointments, setAppointments] = useState([]);
     const [checkInSuccess, setCheckInSuccess] = useState(true);
     const [showCheckInResult, setShowCheckInResult] = useState(false);
     const checkInResultDisplayTime = 5000;
     const checkInRef = React.createRef();
+
+    function requestAllData() {
+        Socket.emit('overview request', {});
+    }
+
+    function establishConnection() {
+        // TODO: jlh29, use this to ensure that the connecting client has the
+        // correct role first
+        requestAllData();
+    }
 
     function updateCheckInSuccess(success) {
         setCheckInSuccess(success);
@@ -15,16 +27,52 @@ export default function LibrarianOverview() {
     }
 
     function onCheckInSubmitClicked() {
-        let checkInID = checkInRef.current.value.strip();
+        let checkInID = checkInRef.current.value.trim();
         if (checkInID.length > 0) {
             Socket.emit('check in', {checkInID}, updateCheckInSuccess);
         }
         checkInRef.current.value = '';
     }
+    
+    function updateAppointments(data) {
+        setAppointments(
+            oldAppointments => {
+                let updatedAppointments = [];
+                updatedAppointments.concat(oldAppointments);
+                for (let newAppointment of data.appointments) {
+                    let isNew = true;
+                    for (let index in updatedAppointments) {
+                        if (updatedAppointments[index].id == newAppointment.id) {
+                            updatedAppointments[index] = newAppointment;
+                            isNew = false;
+                            break;
+                        }
+                    }
+                    if (isNew) {
+                        updatedAppointments.push(newAppointment);
+                    }
+                }
+                return updatedAppointments;
+            }
+        );
+    } 
+
+    function listenToServer() {
+        useEffect(() => {
+            Socket.on('connect', establishConnection);
+            Socket.on('appointments response', updateAppointments);
+            return () => {
+                Socket.off('connect', establishConnection);
+                Socket.off('appointments response', updateAppointments);
+            }
+        });
+    }
+
+    listenToServer();
 
     return (
         <div>
-            <h1>View/Edit Appointments: TODO</h1>
+            <LibrarianAppointmentsOverview appointments={appointments} />
             <h1>View/Edit Rooms: TODO</h1>
             <LibrarianCheckIn
                 inputRef={checkInRef}
