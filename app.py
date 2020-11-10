c598d8e924ac449120005b6541592b7f7e642833
EC2 Default User <ec2-user@ip-172-31-88-9.ec2.internal>
2020-11-10 14:07:55 +0000

    Add event to the calendar, add calendar id to .gitignore

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ dotenv_path = join(dirname(__file__), 'cronofy.env')
 load_dotenv(dotenv_path)
 
 cronofy_access_token = os.environ['ACCESS_TOKEN']
+calendar_id = os.environ['CALENDAR_ID']
 
 database_uri = os.environ['DATABASE_URL']
 
@@ -29,10 +30,30 @@ USERS_UPDATED_CHANNEL = 'users updated'
 
 #Cronofy setup
 cronofy = pycronofy.Client(access_token=cronofy_access_token)
-events = cronofy.read_events()
+events = cronofy.read_events(
+    from_date='2020-11-09',
+    to_date='2020-11-10',
+    tzid='Etc/UTC')
 events_result = events.json()
 
-print(json.dumps(events_result, indent = 2)) 
+# print(json.dumps(events_result, indent = 2)) 
+
+calendars = cronofy.list_calendars()
+print(json.dumps(calendars, indent = 2)) 
+
+#Create events
+event = {
+    'event_id': "ABC123",
+    'summary': "CS490 Project3 Individual deadline",
+    'description': "Finish the individual tasks assigned for MVP",
+    'start': "2020-11-11T15:30:00Z",
+    'end': "2020-11-11T17:00:00Z",
+    'location': {
+        'description': "Slack account"
+    }
+}
+cronofy.upsert_event(calendar_id=calendar_id, event=event)
+
 
 
 @SOCKET.on('connect')
