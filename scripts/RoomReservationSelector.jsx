fcd0a824b8834b16a0839673ad3e95b20d2183d7
jlh29 <jlh29@njit.edu>
2020-11-12 23:36:49 +0000

    Make parameters a bit more clear

--- scripts/RoomReservationSelector.jsx
+++ scripts/RoomReservationSelector.jsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 
 export default function RoomReservationSelector() {
-    const minReservationCount = 2;
-    const maxReservationCount = 12;
+    const minAttendeeCount = 2;
+    const maxAttendeeCount = 12;
 
     return (
         <div>
             <label htmlFor="studentCount">How Many Students (Including You)?</label>
             <select name="studentCount" id="studentCount" defaultValue="-">
-                <option disabled hidden value="-"> - </option>
+                <option disabled hidden value="0"> - </option>
                 {
-                    [...Array(maxReservationCount-minReservationCount+1).keys()].map(
+                    [...Array(maxAttendeeCount - minAttendeeCount + 1).keys()].map(
                         index => {
-                            let value = index + minReservationCount;
+                            let value = index + minAttendeeCount;
                             return <option value={value}>{value}</option>;
                         }
                     )
