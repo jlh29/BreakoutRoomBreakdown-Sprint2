022b1d840019b234a95db22599dedea86c36f44c
jlh29 <jlh29@njit.edu>
2020-11-12 23:49:00 +0000

    Update number of attendees whenever dropdown value is selected

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,13 +1,17 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import RoomReservationSelector from './RoomReservationSelector';
+import RoomReservationAttendeeInput from './RoomReservationAttendeeInput';
 import RoomReservationSubmit from './RoomReservationSubmit';
 
 export function Content() {
+    const [attendeeCount, setAttendeeCount] = useState(0);
+    
     return (
         <div>
             <h1>Webauth Authentication Service</h1>
             <GoogleButton />
-            <RoomReservationSelector />
+            <RoomReservationSelector setAttendeeCount={setAttendeeCount} />
+            <RoomReservationAttendeeInput attendeeCount={attendeeCount} />
             <RoomReservationSubmit />
         </div>
     );
