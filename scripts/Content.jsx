00e9aa7e2a86d60da8d47b86e82cbbe4c7ae0992
jlh29 <jlh29@njit.edu>
2020-11-12 23:49:00 +0000

    Update number of attendees whenever dropdown value is selected

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,11 +1,15 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import RoomReservationSelector from './RoomReservationSelector';
+import RoomReservationAttendeeInput from './RoomReservationAttendeeInput';
 import RoomReservationSubmit from './RoomReservationSubmit';
 
 export function Content() {
+    const [attendeeCount, setAttendeeCount] = useState(0);
+    
     return (
         <div>
-            <RoomReservationSelector />
+            <RoomReservationSelector setAttendeeCount={setAttendeeCount} />
+            <RoomReservationAttendeeInput attendeeCount={attendeeCount} />
             <RoomReservationSubmit />
         </div>
     );
--- scripts/RoomReservationSelector.jsx
+++ scripts/RoomReservationSelector.jsx
@@ -1,19 +1,29 @@
 import * as React from 'react';
 
-export default function RoomReservationSelector() {
+export default function RoomReservationSelector(props) {
     const minAttendeeCount = 2;
     const maxAttendeeCount = 12;
+    const { setAttendeeCount } = props;
+
+    function updateAttendeeCount(event) {
+        setAttendeeCount(parseInt(event.target.value));
+    }
 
     return (
         <div>
             <label htmlFor="studentCount">How Many Students (Including You)?</label>
-            <select name="studentCount" id="studentCount" defaultValue="-">
+            <select
+                name="studentCount"
+                id="studentCount"
+                defaultValue="0"
+                onChange={updateAttendeeCount}
+            >
                 <option disabled hidden value="0"> - </option>
                 {
                     [...Array(maxAttendeeCount - minAttendeeCount + 1).keys()].map(
                         index => {
                             let value = index + minAttendeeCount;
-                            return <option value={value}>{value}</option>;
+                            return <option value={value} key={value}>{value}</option>;
                         }
                     )
                 }
