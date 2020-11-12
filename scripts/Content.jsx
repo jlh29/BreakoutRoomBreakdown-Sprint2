ea94c9b5a4554f6e4395fae44776e4b49d74f6ab
jlh29 <jlh29@njit.edu>
2020-11-12 23:58:14 +0000

    Create dynamic list of input boxes that updates an Attendees state when modified

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -5,13 +5,17 @@ import RoomReservationSubmit from './RoomReservationSubmit';
 
 export function Content() {
     const [attendeeCount, setAttendeeCount] = useState(0);
-    
+    const [attendees, setAttendees] = useState([]);
+
     return (
         <div>
             <h1>Webauth Authentication Service</h1>
             <GoogleButton />
             <RoomReservationSelector setAttendeeCount={setAttendeeCount} />
-            <RoomReservationAttendeeInput attendeeCount={attendeeCount} />
+            <RoomReservationAttendeeInput 
+                attendeeCount={attendeeCount}
+                setAttendees={setAttendees}
+            />
             <RoomReservationSubmit />
         </div>
     );
