96c9e9e053ab270dafe421e563b128b8a90614e3
jlh29 <jlh29@njit.edu>
2020-11-13 06:11:55 +0000

    Obtain and send back available dates near a requested date (i.e. on load)

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 import flask
 import flask_socketio
+import db_instance
+from db_instance import DB
 import db_utils
-from db_utils import DB
 import models 
 import socket_utils
 from socket_utils import SOCKET
@@ -22,6 +23,14 @@ USER_LOGIN_NAME_KEY = "name"
 USER_LOGIN_EMAIL_KEY = "email"
 
 SUCCESSFUL_LOGIN_CHANNEL = "successful login"
+
+TIME_AVAILABILITY_REQUEST_CHANNEL = "time availability request"
+TIME_AVAILABILITY_RESPONSE_CHANNEL = "time availability response"
+ALL_TIMES_KEY = "times"
+
+DATE_AVAILABILITY_REQUEST_CHANNEL = "date availability request"
+DATE_AVAILABILITY_RESPONSE_CHANNEL = "date availability response"
+ALL_DATES_KEY = "dates"
 
 CONNECTED_USERS = {}
 
@@ -45,42 +54,31 @@ def on_new_user_login(data):
         room=flask.request.sid,
     )
 
-@SOCKET.on("date availability")
-def on_date_availability(data):
+@SOCKET.on(DATE_AVAILABILITY_REQUEST_CHANNEL)
+def on_date_availability_request(data):
     print("Got an event for date input with data:", data)
-    date = datetime.datetime.strptime(data["date"], "%Y-%m-%dT%H:%M:%S.%fZ")
-    day = str(date.date())
-    print(date.date())
-    
-    #mock avaiable dates
-    available_list = {"2020-11-13": ["9:00-11:00", "1:00-3:00"]}
-    
-    if day in available_list:
-        print(day, "is available")
-        
-        for i in range(len(available_list[day])):
-            SOCKET.emit(
-                "date status",
-                {
-                 "time available": available_list[day][i],
-                })
-    else:
-        print(day, "is not available")
-        SOCKET.emit("date status", {"is_available": False})
-        
-@SOCKET.on("time availability")
-def on_time_availability(data):
-    print("Got an event for time input with data:", data)
-    time = data["time"]
-    
-    print(time)
+    date = datetime.datetime.fromtimestamp(data["date"] / 1000.0)
+    available_dates = db_utils.get_available_dates_after_date(
+        date=date,
+        date_range=3,
+    )
+    print(available_dates)
+    available_date_timestamps = [
+        available_date.timestamp() * 1000.0
+        for available_date in available_dates
+    ]
+    SOCKET.emit(
+        DATE_AVAILABILITY_RESPONSE_CHANNEL,
+        {"dates": available_date_timestamps},
+        room=flask.request.sid,
+    )
 
 @APP.route("/")
 def index():
     return flask.render_template("index.html")
 
 if __name__ == "__main__":
-    db_utils.init_db(APP)
+    db_instance.init_db(APP)
     socket_utils.init_socket(APP)
     SOCKET.run(
         APP,
