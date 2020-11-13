fc1538b404285438c00c4e89e87a2027d85c616a
jlh29 <jlh29@njit.edu>
2020-11-13 01:48:59 +0000

    Add simple tracking of logged in users

--- app.py
+++ app.py
@@ -10,69 +10,81 @@ import models
 import socket_utils
 from socket_utils import SOCKET
 
-load_dotenv(join(dirname(__file__), 'sql.env'))
+load_dotenv(join(dirname(__file__), "sql.env"))
 
 APP = flask.Flask(__name__)
-APP.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+APP.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 
-USERS_UPDATED_CHANNEL = 'users updated'
+USERS_UPDATED_CHANNEL = "users updated"
 
-@SOCKET.on('connect')
+USER_LOGIN_CHANNEL = "new login"
+USER_LOGIN_NAME_KEY = "name"
+USER_LOGIN_EMAIL_KEY = "email"
+
+SUCCESSFUL_LOGIN_CHANNEL = "successful login"
+
+CONNECTED_USERS = {}
+
+@SOCKET.on("connect")
 def on_connect():
-    print('Someone connected!')
+    print("Someone connected!")
     
-@SOCKET.on('disconnect')
+@SOCKET.on("disconnect")
 def on_disconnect():
-    print ('Someone disconnected!')
+    print ("Someone disconnected!")
+    CONNECTED_USERS.pop(flask.request.sid, None)
     
-@SOCKET.on('new username')
-def on_new_google_user(data):
-    print("Got an event for new google user input with data:", data)
-    name=data['name']
-    SOCKET.emit('username', {
-        'names': name
-    })
+@SOCKET.on(USER_LOGIN_CHANNEL)
+def on_new_user_login(data):
+    print(f"Got an event for new user login with data: {data}")
+    # TODO: jlh29, update this
+    CONNECTED_USERS[flask.request.sid] = "new user"
+    SOCKET.emit(
+        SUCCESSFUL_LOGIN_CHANNEL,
+        {USER_LOGIN_NAME_KEY: data[USER_LOGIN_NAME_KEY]},
+        room=flask.request.sid,
+    )
 
-@SOCKET.on('date availability')
+@SOCKET.on("date availability")
 def on_date_availability(data):
     print("Got an event for date input with data:", data)
-    date = datetime.datetime.strptime(data['date'], '%Y-%m-%dT%H:%M:%S.%fZ')
+    date = datetime.datetime.strptime(data["date"], "%Y-%m-%dT%H:%M:%S.%fZ")
     day = str(date.date())
     print(date.date())
     
     #mock avaiable dates
-    available_list = {"2020-11-13": ['9:00-11:00', '1:00-3:00']}
+    available_list = {"2020-11-13": ["9:00-11:00", "1:00-3:00"]}
     
     if day in available_list:
         print(day, "is available")
         
         for i in range(len(available_list[day])):
             SOCKET.emit(
-                'date status', 
+                "date status",
                 {
-                 'time available': available_list[day][i],
+                 "time available": available_list[day][i],
                 })
     else:
         print(day, "is not available")
         SOCKET.emit("date status", {"is_available": False})
         
-@SOCKET.on('time availability')
+@SOCKET.on("time availability")
 def on_time_availability(data):
     print("Got an event for time input with data:", data)
-    time = data['time']
+    time = data["time"]
     
     print(time)
 
-@APP.route('/')
+@APP.route("/")
 def index():
     return flask.render_template("index.html")
 
-if __name__ == '__main__': 
+if __name__ == "__main__":
     db_utils.init_db(APP)
     socket_utils.init_socket(APP)
     SOCKET.run(
         APP,
-        host=os.getenv('IP', '0.0.0.0'),
-        port=int(os.getenv('PORT', '8080')),
+        host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "8080")),
         debug=True
     )
