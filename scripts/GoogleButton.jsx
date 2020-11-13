5164e12946ad70558bad75e658f173eb2d6f4930
jlh29 <jlh29@njit.edu>
2020-11-13 01:35:10 +0000

    Fix issues with disallowing non-NJIT emails to sign in

--- scripts/GoogleButton.jsx
+++ scripts/GoogleButton.jsx
@@ -1,28 +1,35 @@
 import * as React from 'react';
-import { Socket }from './Socket';
+import Socket from './Socket';
 import ReactDOM from 'react-dom';
 import GoogleLogin from 'react-google-login';
-import { Content_Auth } from './Content_Auth';
+import Content_Auth from './Content_Auth';
 
-
-function handleSubmit(response) {
-  console.log(response)
-  let username = response.profileObj.name;
-  Socket.emit('new username', {
-    'name': username,
-  });
+export default function GoogleButton() {
+    function handleSuccess(response) {
+        if (!('profileObj' in response)) {
+            handleFailure(response);
+            return;
+        }
+        let { name, email } = response.profileObj;
+        if (!email.toLowerCase().endsWith('@njit.edu')) {
+            handleFailure(response);
+            return;
+        }
+        Socket.emit('new login', {name, email});
+        console.log(`Sent the name ${name} and email ${email} to server!`);
+    }
     
-    console.log('Sent the name ' + username + ' to server!');
-    ReactDOM.render(<Content_Auth />, document.getElementById('content'));
-}
+    function handleFailure(response) {
+        // TODO: show some sort of error message
+        console.log('Encountered an error while signing in.');
+    }
 
-export function GoogleButton() {
-  return <GoogleLogin
-    className="gbutton"
-    clientId="791154624378-g41rhk0tetto6ueot7mcjffts9g294e7.apps.googleusercontent.com"
-    buttonText="Login"
-    onSuccess={handleSubmit}
-    onFailure={handleSubmit}
-    fields="name"
-    cookiePolicy={'single_host_origin'}/>;
+    return <GoogleLogin
+        className="gbutton"
+        clientId="791154624378-g41rhk0tetto6ueot7mcjffts9g294e7.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={handleSuccess}
+        onFailure={handleFailure}
+        cookiePolicy={'single_host_origin'}
+    />;
 }
