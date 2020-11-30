7df970345074a6cd57b06f804aec5078c3229d6a
bellesune <bcs32@njit.edu>
2020-11-30 21:33:23 +0000

    Accept props from Reservation Overview

--- scripts/ReservationUsersNumber.jsx
+++ scripts/ReservationUsersNumber.jsx
@@ -1,23 +1,19 @@
 import * as React from 'react';
 
-export default function ReservationUsersNumber() {
-    const [number, setNumber] = React.useState();
-    
-    function handleChange(event){
-        setNumber(event.target.value);
-    }
+export default function ReservationUsersNumber(props) {
+    const { newNumber } = props;
     
     return(
         <div>
             <label>Mobile number: </label>
             <input 
             type="tel" 
-            id="number"
+            id="mobileNumber"
             name="number"
-            placeholder="2012345678"
+            value={newNumber}
+            placeholder="2014448888"
             maxlength="10"
             pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-            onChange={handleChange}
             required/>
         </div>
     );
