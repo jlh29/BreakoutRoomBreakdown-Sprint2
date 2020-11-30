cc849a2b740e1055f21d11104e869efdccddc54d
EC2 Default User <ec2-user@ip-172-31-88-9.ec2.internal>
2020-11-30 21:21:34 +0000

    Update the message with appointment details

--- api_twilio.py
+++ api_twilio.py
@@ -7,18 +7,25 @@ class Twilio:
     dotenv_path = join(dirname(__file__), "twilio.env")
     load_dotenv(dotenv_path)
     
-    twilio_account_sid = os.environ['TWILIO_ACCOUNT_SID']
-    twilio_auth_token = os.environ['TWILIO_AUTH_TOKEN']
+    TWILIO_ACCOUNT = os.environ['TWILIO_ACCOUNT_SID']
+    TWILIO_AUTH = os.environ['TWILIO_AUTH_TOKEN']
     
     twilio_phone = os.environ['TWILIO_PHONE']
     my_phone = os.environ['MY_PHONE']
     
-    client = Client(twilio_account_sid, twilio_auth_token)
+    def __init__(self, number):
+        self.to_number = number
     
-    message = client.messages \
-                    .create(
-                         body="Hello World from Twilio!",
-                         from_=twilio_phone, #twilio phone number
-                         to=my_phone  #my real phone number
-                     )
-   
+    # send reservation details: date, time, attendees, confirmation number
+    def send_text(self, date, time, attendees, confirmation):
+        client = Client(self.TWILIO_ACCOUNT, self.TWILIO_AUTH)
+        
+        message = client.messages.create(
+            body=" \
+            Good day! You made a reservation at NJIT Library" \
+            "on {} at {} with {}. Your confirmation number is {}" \
+            .format(date, time, attendees, confirmation),
+            
+            from_=self.twilio_phone, #twilio phone number
+            to=self.to_number  #receiver's number
+            )
