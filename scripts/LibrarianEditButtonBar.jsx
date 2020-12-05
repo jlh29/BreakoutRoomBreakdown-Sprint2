f9702d9db8e5e98b5bd8f4d771e1252e919e0b69
jlh29 <jlh29@njit.edu>
2020-12-05 11:55:14 +0000

    Lint all LibrarianEdit... files

--- scripts/LibrarianEditButtonBar.jsx
+++ scripts/LibrarianEditButtonBar.jsx
@@ -1,29 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import LibrarianEditConfirmButton from './LibrarianEditConfirmButton';
 import LibrarianEditCancelButton from './LibrarianEditCancelButton';
 import LibrarianEditButton from './LibrarianEditButton';
 
 export default function LibrarianEditButtonBar(props) {
-    const {
-        onConfirmClick,
-        onCancelClick,
-        isEditing,
-        enableEditing,
-        disableEditing,
-    } = props;
-    
-    if (isEditing) {
-        return (
-            <div className='librarianEditButtonBar'>
-                <LibrarianEditConfirmButton onConfirmClick={onConfirmClick} />
-                <LibrarianEditCancelButton onCancelClick={disableEditing} />
-            </div>
-        );
-    } else {
-        return (
-            <div className='librarianEditButtonBar'>
-                <LibrarianEditButton onEditClick={enableEditing} />
-            </div>
-        );
-    }
+  const {
+    onConfirmClick,
+    isEditing,
+    enableEditing,
+    disableEditing,
+  } = props;
+
+  if (isEditing) {
+    return (
+      <div className="librarianEditButtonBar">
+        <LibrarianEditConfirmButton onConfirmClick={onConfirmClick} />
+        <LibrarianEditCancelButton onCancelClick={disableEditing} />
+      </div>
+    );
+  }
+  return (
+    <div className="librarianEditButtonBar">
+      <LibrarianEditButton onEditClick={enableEditing} />
+    </div>
+  );
 }
+
+LibrarianEditButtonBar.propTypes = {
+  onConfirmClick: PropTypes.func.isRequired,
+  isEditing: PropTypes.bool.isRequired,
+  enableEditing: PropTypes.func.isRequired,
+  disableEditing: PropTypes.func.isRequired,
+};
--- scripts/LibrarianEditCancelButton.jsx
+++ scripts/LibrarianEditCancelButton.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default function LibrarianEditCancelButton(props) {
-    const { onCancelClick } = props;
-    
-    return (
-        <button
-            type='button'
-            className='librarianEditCancelButton'
-            onClick={onCancelClick}
-        >Discard Changes</button>
-    );
+  const { onCancelClick } = props;
+
+  return (
+    <button
+      type="button"
+      className="librarianEditCancelButton"
+      onClick={onCancelClick}
+    >
+      Discard Changes
+    </button>
+  );
 }
+
+LibrarianEditCancelButton.propTypes = {
+  onCancelClick: PropTypes.func.isRequired,
+};
