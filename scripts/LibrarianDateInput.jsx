d63143c8708ebb2646d1592d2f767c35c640980a
bellesune <bcs32@njit.edu>
2020-12-06 08:06:21 +0000

    Create event handler for dates and note, reformat date string

--- scripts/LibrarianDateInput.jsx
+++ scripts/LibrarianDateInput.jsx
@@ -1,6 +1,26 @@
 import * as React from 'react';
+import LibrarianDisableButton from './LibrarianDisableButton';
 
 export default function LibrarianDateInput(props) {
+  const [startDate, setStartDate] = React.useState("");
+  const [endDate, setEndDate] = React.useState("");
+  const [note, setNote] = React.useState("");
+  
+  const handleStart = ({ target }) => {
+    const newText = target.value;
+    setStartDate(newText);
+  };
+  
+  const handleEnd = ({ target }) => {
+    const newText = target.value;
+    setEndDate(newText);
+   
+  };
+  
+  const handleNote = ({ target }) => {
+    const newText = target.value;
+    setNote(newText);
+  };
  
   return (
     <div>
@@ -8,15 +28,27 @@ export default function LibrarianDateInput(props) {
         <input
           id="librarianStartDate"
           type="date"
-          name="startDate"
+          value={startDate.toString("yyyy-MM-dd")}
+          onChange={handleStart}
         />
         
         <label for="librarianEndDate">End date:</label>
         <input
           id="librarianEndDate"
           type="date"
-          name="endDate"
+          value={endDate.toString("yyyy-MM-dd")}
+          onChange={handleEnd}
         />
+        
+        <label for="librarianEndDate">Note:</label>
+        <input
+          id="librarianNote"
+          type="text"
+          value={note}
+          onChange={handleNote}
+        />
+        
+        <LibrarianDisableButton startDate={startDate} endDate={endDate} note={note}/>
     </div>
   );
 }
