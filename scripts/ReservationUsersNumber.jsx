5d26bf6112640d742a98a71021c216a362b761c1
bellesune <bcs32@njit.edu>
2020-12-08 02:48:31 +0000

    Remove value attribute

--- scripts/ReservationUsersNumber.jsx
+++ scripts/ReservationUsersNumber.jsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 
 export default function ReservationUsersNumber(props) {
-    const { newNumber } = props;
-    
+
     return(
         <div>
             <label>Mobile number: </label>
@@ -10,9 +9,8 @@ export default function ReservationUsersNumber(props) {
             type="tel" 
             id="mobileNumber"
             name="number"
-            value={newNumber}
             placeholder="2014448888"
-            maxlength="10"
+            maxLength="10"
             pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
             required/>
         </div>
