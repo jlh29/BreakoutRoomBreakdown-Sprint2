a0ee7451cb70ca5d409682e271d85055fcb62907
DP <ec2-user@ip-172-31-30-62.ec2.internal>
2020-12-08 07:51:18 +0000

    added back button on the landing page to go back to Normal app

--- scripts/LandingPage.jsx
+++ scripts/LandingPage.jsx
@@ -2,11 +2,18 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import { ReservationOverview } from './ReservationOverview';
 
+function goBack(){
+        ReactDOM.render(<ReservationOverview />, document.getElementById('content'));
+    }
 
 export function LandingPage() {
-    
+
+
   return (
       <div className="all">
+      <form id="back" onClick={goBack}>
+        <button>Back to BNB</button>
+      </form>
       <p1>Team Members:</p1>
       </div>);
 }
