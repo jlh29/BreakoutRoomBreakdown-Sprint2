5be5583e455c40f9816a66a31395828be3fd94d4
jlh29 <jlh29@njit.edu>
2020-12-08 23:33:08 +0000

    Change the Google OAuth ID

--- scripts/GoogleButton.jsx
+++ scripts/GoogleButton.jsx
@@ -42,7 +42,7 @@ export default function GoogleButton() {
   return (
     <GoogleLogin
       className="gbutton"
-      clientId="791154624378-g41rhk0tetto6ueot7mcjffts9g294e7.apps.googleusercontent.com"
+      clientId="650589780426-q6cu2s43su3mba4u4948bk0gcdsaht39.apps.googleusercontent.com"
       buttonText="Login"
       onSuccess={handleSuccess}
       onFailure={handleFailure}
