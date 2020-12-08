6a253176f0afe7b5c626fbcf940d4869562dee9f
DP <ec2-user@ip-172-31-30-62.ec2.internal>
2020-12-08 07:48:11 +0000

    way to LandingPage

---
+++ scripts/LandingPage.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { ReservationOverview } from './ReservationOverview';
+
+
+export function LandingPage() {
+    
+  return (
+      <div className="all">
+      <p1>Team Members:</p1>
+      </div>);
+}
