eb77103bb9943dfddecbb674484aaa6340c55e9e
jlh29 <jlh29@njit.edu>
2020-12-09 02:14:05 +0000

    Fix all bugs keeping script.js from compiling

--- scripts/LibrarianDisableButton.jsx
+++ scripts/LibrarianDisableButton.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Socket } from './Socket';
+import Socket from './Socket';
 
 export default function LibrarianDisableButton(props) {
     const {startDate} = props;
